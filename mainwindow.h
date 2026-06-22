f5b6578e483de115a9cac3dd6ecd2ff99c928395
David Burchakov <davidburchakov@gmail.com>
2026-06-22 20:07:37 +0200

    successful setup

---
+++ mainwindow.h
@@ -0,0 +1,29 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTreeWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow() override;
+
+private slots:
+    void onTextChanged();
+    void buildAST(const std::string &code);
+
+private:
+    Ui::MainWindow *ui;
+
+    void addNode(QTreeWidgetItem *parent,
+                 const std::string &text);
+};
+
+#endif
